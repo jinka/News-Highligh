df8bac504f4beee7f3719cb47040ba953d77ccad
jinka <voiphandy@gmail.com>
2019-02-05 11:36:34 +0300

    Deploying to Heoku

--- app/config.py
+++ app/config.py
@@ -7,6 +7,7 @@ class Config:
     NEWS_SOURCES_BASE_URL ='https://newsapi.org/v2/sources?language=en&category={}&apiKey={}'
     ARTICLES_BASE_URL = 'https://newsapi.org/v2/everything?language=en&sources={}&apiKey={}'
     NEWS_API_KEY='7898c1e92e3f4963b15bf09d8dcb0c15'
+    
     # @staticmethod
     # def init_app(app):
     #     pass
