d4c442fa2f67d2a20be842c8b87f7af2ee7e7af5
jinka <voiphandy@gmail.com>
2019-02-05 11:45:30 +0300

    Deploying to Heoku

--- app/config.py
+++ app/config.py
@@ -8,9 +8,7 @@ class Config:
     ARTICLES_BASE_URL = 'https://newsapi.org/v2/everything?language=en&sources={}&apiKey={}'
     NEWS_API_KEY='7898c1e92e3f4963b15bf09d8dcb0c15'
     
-    # @staticmethod
-    # def init_app(app):
-    #     pass
+   
 class ProdConfig(Config):
     '''
     Production  configuration child class
