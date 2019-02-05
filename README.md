63edf7c55a8b2a8535d26121927d0f7fd4f73cac
jinka <voiphandy@gmail.com>
2019-02-05 10:05:51 +0300

    Fetching source from the news highlights

---
+++ README.md
@@ -0,0 +1,38 @@
+# Project Title
+# News Highlight
+This a web application and was generated with Python 3.
+
+# Project Description
+User can be upto date for the latest news from different sources around the world. 
+
+# Setup/Installation Requirements
+1. Internet connection.
+2. Python 3 installation.
+3. Pip Installation.
+4. Clone https://github.com/jinka/News-Highligh.git
+5. Go inside the folder that contains the project.
+6. Run `./run.py`  Navigate to the menu.
+
+# Specifications
+|GIVEN|WHEN|THEN|
+|-----|------|-------|
+|News home page with sources load|App load|Source categories displayed|
+|List of aricles|Click source news|Displaying the clicked news link|
+|Display preview the article|Loading page|Article with image and description|
+|Read the Article|Click home page|home page load|
+
+# Bugs/Contribution
+Currently no know bug.
+To Contribute:
+1. Fork this repo:https://github.com/jinka/News-Highligh
+2. Clone this repo to your machine:https://github.com/jinka/News-Highligh.git
+
+# Author information
+My name is Dayud Mohamed Farah, I am one the Moringa Core class 14.
+
+# Contact Information
+Dayud Mohamed Farah Tel.: +254 716 761 606 Email: voiphandy@gmail.company
+
+# License and Copyright Information
+
+This app is licensed under the terms of MIT, an Copyright by Moringa School core class 14.
--- app/models.py
+++ app/models.py
@@ -2,14 +2,14 @@ class Articles:
     '''
     Articles class to define articles objects
     '''
-    # def __init__(self,id,author,title,description,url,image,date):
-    #     self.id = id
-    #     self.author = author
-    #     self.title = title
-    #     self.description = description
-    #     self.url = url
-    #     self.image = image
-    #     self.date = date
+    def __init__(self,id,author,title,description,url,image,date):
+        self.id = id
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.image = image
+        self.date = date
 
         
 class Sources:
@@ -17,12 +17,12 @@ class Sources:
     Sources class to define Sources Objects
     '''
 
-    # def __init__(self,id,name,description,url,category,country,language):
-    #     self.id =id
-    #     self.name = name
-    #     self.description = description
-    #     self.url = url
-    #     self.category = category
-    #     self.country = country
-    #     self.language = language
+    def __init__(self,id,name,description,url,category,country,language):
+        self.id =id
+        self.name = name
+        self.description = description
+        self.url = url
+        self.category = category
+        self.country = country
+        self.language = language
     
