7f9581a80d792f15a8a26bb839a620efda36eb9a
jinka <voiphandy@gmail.com>
2019-02-06 03:59:01 +0300

    Add styling in base and macros

--- app/config.py
+++ app/config.py
@@ -8,7 +8,7 @@ class Config:
     ARTICLES_BASE_URL = 'https://newsapi.org/v2/everything?language=en&sources={}&apiKey={}'
     NEWS_API_KEY='7898c1e92e3f4963b15bf09d8dcb0c15'
     
-   
+
 class ProdConfig(Config):
     '''
     Production  configuration child class
