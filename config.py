bbaa93eb937209c5edc9dfc351306f03f85235e7
jinka <voiphandy@gmail.com>
2019-02-07 13:25:03 +0300

    Moved config.py from app folder to the root

--- config.py
+++ config.py
@@ -8,8 +8,6 @@ class Config:
     ARTICLES_BASE_URL = 'https://newsapi.org/v2/everything?language=en&sources={}&apiKey={}'
     NEWS_API_KEY=os.environ.get('NEWS_API_KEY')
     SECRET_KEY = os.environ.get('SECRET_KEY')    
-    #'7898c1e92e3f4963b15bf09d8dcb0c15'
-
 
 
 class ProdConfig(Config):
